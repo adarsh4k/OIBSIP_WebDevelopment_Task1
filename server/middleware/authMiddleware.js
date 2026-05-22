eba355227428ff51a236d8f90b33389cbe69f9df
unknown <adarsh4k.in@gmail.com>
2026-05-22 16:33:50 +0530

    solved orders displaying issue

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+/*const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
@@ -16,5 +16,40 @@ module.exports = (req, res, next) => {
 
   } catch (error) {
     res.status(401).json({ msg: "Invalid token" });
+  }
+};*/
+
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        msg: "No token provided"
+      });
+    }
+
+    // ✅ REMOVE "Bearer "
+    const token = authHeader.startsWith("Bearer ")
+      ? authHeader.split(" ")[1]
+      : authHeader;
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(401).json({
+      msg: "Invalid token"
+    });
   }
 };
